2f290ca7302fa0b2d0605cb30c6c32f5eea75621
kenshi.ueda <Kenshi150107@gmail.com>
2021-02-21 08:27:04 +0900

    編集１

---
+++ taskApp/inputViewController.swift
@@ -0,0 +1,20 @@
+//
+//  inputViewController.swift
+//  taskApp
+//
+//  Created by ueda on 2021/02/19.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
